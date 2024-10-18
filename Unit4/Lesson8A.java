8c1ee1ae9df6510d929686ba031affb27361cd2c
andyydell <25mdellorfano@derryfield.org>
2024-10-18 08:50:34 -0400

    renamed

--- Unit4/Lesson8A.java
+++ Unit4/Lesson8A.java
@@ -1,10 +1,11 @@
-package Unit3;
+package Unit4;
 //Andy Dell'Orfano
 //Assignment Part A
 //10/18/24
 import java.util.Scanner;
-public class Lesson9A {
+public class Lesson8A {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         
     }
 }
