fd2a72a1c6c6e4c0ce77fd8de625e80f00c52d24
andyydell <25mdellorfano@derryfield.org>
2024-10-18 09:52:30 -0400

    Update Lesson8A.java

--- Unit4/Lesson8A.java
+++ Unit4/Lesson8A.java
@@ -6,6 +6,12 @@ import java.util.Scanner;
 public class Lesson8A {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        double percent = 100;
+        int years = 0;
+        while (percent >= 1){
+            years = years +30;
+            percent = (percent/2);
+        }
+        System.out.println("It will take " + years + " years to decay to less than 1% and there will be " + percent + "% of the material remaining.");
     }
 }
