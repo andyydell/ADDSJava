c9eea30b7584d4d464c93603e75e83be172d014f
andyydell <25mdellorfano@derryfield.org>
2024-10-23 23:35:23 -0400

    A lot

--- Unit4/Lesson8B.java
+++ Unit4/Lesson8B.java
@@ -2,8 +2,24 @@ package Unit4;
 //Andy Dell'Orfano
 //10/18/24
 //PartB
+import java.util.Scanner;
 public class Lesson8B {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the APR? ");
+        double apr = input.nextDouble();
+        System.out.print("What is the initial balance? ");
+        double balance = input.nextDouble();
+        int years = 1;
+        System.out.println("Year |      Int Earned      |       Balance($)      ");
+        System.out.println("----------------------------------------------------");
+        double interest;
+        while (years <= 5) {
+            interest = balance * (apr/100);
+            System.out.printf(" %d   | %13.2f        | %15.2f     %n", years, interest, balance );
+            balance = balance *(1+(apr/100));
+            years = years + 1;
+        }
+
     }
 }
