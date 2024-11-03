6b438f212526c101e722c52cced4a1290ae8d428
andyydell <25mdellorfano@derryfield.org>
2024-11-03 18:16:31 -0500

    e

---
+++ InClass/Halloween.java
@@ -0,0 +1,9 @@
+package InClass;
+
+public class Halloween {
+    public static void main(String[] args) {
+        int a;
+        double x = 3.1;
+        a = (int)x;
+    }
+}
---
+++ Unit4/Lesson10a.java
@@ -0,0 +1,10 @@
+package Unit4;
+
+public class Lesson10a {
+    public static void main(String[] args) {
+        String name = "Harry";
+        System.out.println(name.charAt(name.length()));
+        System.out.println("The last letter of the name is ");
+}
+
+}
