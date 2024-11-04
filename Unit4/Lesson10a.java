2d6aa58c0b0163ed1500523392867427cb469abd
andyydell <25mdellorfano@derryfield.org>
2024-11-03 20:33:09 -0500

    Update Lesson10a.java

--- Unit4/Lesson10a.java
+++ Unit4/Lesson10a.java
@@ -2,9 +2,15 @@ package Unit4;
 
 public class Lesson10a {
     public static void main(String[] args) {
-        String name = "Harry";
+        /*String name = "Harry";
         System.out.println(name.charAt(name.length()));
         System.out.println("The last letter of the name is ");
+        */
+        //This code carries a run time error as when the code begins running, it will have an issue when it gets to determining which character exists there
+        //The fix is to change it to char - 1 as the length is greater than the character identifier of the last letter
+
+        String name = "Harry";
+        System.out.println("The last letter of the name is " + (name.charAt(name.length() - 1)));
 }
 
 }
