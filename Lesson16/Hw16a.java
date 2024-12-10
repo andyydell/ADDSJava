c86eedf18b5d8bd7feebc06a3ed2530eef3f9284
andyydell <25mdellorfano@derryfield.org>
2024-12-10 10:46:04 -0500

    e

--- Lesson16/Hw16a.java
+++ Lesson16/Hw16a.java
@@ -7,21 +7,23 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 public class Hw16a {
-    public static double[] dropScore(double[] array, double term){
+    public static double[] dropScore(double[] array, int term){
         int count = 0;
         int index = 0;
-        for (int i =0; i<array.length; i++ ){
-            if(array[i] == term){
-                count ++;
+        double MOE = .01;
+        for (int i =1; i<array.length; i++ ){
+            if(i == term){
+                count++;
             }
         }
         if (count == 0){
+            System.out.println("We have not taken that many quizes!");
             return array;
         } else {
             double[] finalArray = new double[array.length-1];
-            for (int i=0; i<array.length; i++){
-                if (array[i] != term){
-                    finalArray[index++] = array[i];
+            for (int i=1; i<array.length; i++){
+                if (i != term){
+                    finalArray[index++] = array[i-1];
                 }
             }
             return finalArray;
@@ -30,34 +32,42 @@ public class Hw16a {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         double[] scores = new double[20];
-
+        int currentSize = 0;
         System.out.print("Enter upt to 20 quiz scores or q to quit: ");
         for (int i=0; i <=20; i++){
             if (in.hasNext("q") || in.hasNext("Q")){
+                in.next();
                 break;
-            } else {
+            } else if (in.hasNextDouble()) {
                 scores[i] = in.nextDouble();
+                currentSize++;
+            } else {
+                in.next();
             }
         }
-        for (int i =0; i<scores.length; i++){
+        System.out.println("\nYour quiz scores are: ");
+        for (int i =0; i<currentSize; i++){
             int x = i+1;
             System.out.println("Quiz " + x + ": " + scores[i]);
         }
-        System.out.print("Would you like to drop a score? If yes enter y: ");
-        if (in.hasNext("y") || in.hasNext("Y")){
-            System.out.print("Which one? ");
-            double score = in.nextDouble();
-            dropScore(scores, score);
-            for (int i =0; i<scores.length; i++){
+        in.nextLine();
+        System.out.print("\nWould you like to drop a score? If yes enter y: ");
+        
+        String response = in.nextLine();
+        if (response.equalsIgnoreCase("y")){
+            System.out.print("Which quiz number? ");
+            int score = in.nextInt();
+            double[] scoresNew = new double[20];
+            scoresNew = dropScore(scores, score);
+
+            for (int i =0; i<currentSize-1; i++){
                 int x = i+1;
-                System.out.println("Quiz " + x + ": " + scores[i]);
-                in.next();
+                System.out.println("Quiz " + x + ": " + scoresNew[i]);
             }
         } else {
-             for (int i =0; i<scores.length; i++){
+             for (int i =0; i<currentSize; i++){
                 int x = i+1;
                 System.out.println("Quiz " + x + ": " + scores[i]);
-                in.next();
             }
         }
     }
