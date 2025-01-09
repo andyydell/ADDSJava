eab420f8e1d99fb8652b8b891547f4f61dc79e97
andyydell <25mdellorfano@derryfield.org>
2025-01-08 22:05:05 -0500

    e

---
+++ InClass(1-8)/Hw20A.java
@@ -0,0 +1,26 @@
+//Andy Dell'Orfano
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Hw20A {
+    public static void main(String[] args) throws FileNotFoundException {
+        File inputFile = new File("consumerData.txt");
+        Scanner in = new Scanner(inputFile);
+         in.useDelimiter("[,\\s\\n]+");
+        System.out.println("---------------------------------------------");
+        System.out.println("ID     |            Name |          Balance |");
+        System.out.println("---------------------------------------------");
+        int count = 0;
+        while (in.hasNext()) {
+            count++;
+            int Id = in.nextInt();       
+            String name = in.next();            
+            double balance = in.nextDouble();   
+            System.out.printf("%06d | %15s | $%15.2f |%n", Id, name, balance);
+        }
+
+    }
+}
+
+
---
+++ InClass(1-8)/Hw20B.java
@@ -0,0 +1,29 @@
+//Andy Dell'Orfano
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Hw20B {
+    public static void main(String[] args) throws FileNotFoundException {
+        File inputFile = new File("characters.txt");
+        Scanner in = new Scanner(inputFile);
+            in.useDelimiter("");
+       int count = 0;
+       int dCount = 0;
+       int lCount = 0;
+       while (in.hasNext()) {
+            if(in.hasNextDouble()){
+                dCount++;
+            } else {
+                lCount++;
+            }
+            char c = in.next().charAt(0);
+            System.out.print(c);
+            count++;
+       }
+       System.out.printf("Count = %d%nDigits = %d%nLetters = %d%n", count, dCount, lCount);
+   
+
+    }
+}
