f132bf812b58356a08d11e5544f0a409d4ca078e
andyydell <25mdellorfano@derryfield.org>
2025-02-03 19:07:11 -0500

    e

---
+++ Hw28/Hw28.java
@@ -0,0 +1,11 @@
+//Andy Dell'Orfano
+//2/3/25
+
+package Hw28;
+
+public class Hw28 {
+    public static void main(String[] args) {
+        //Reptile is super class because the other classes are types of reptiles
+        //The answer is D because instrument as a class is too general and tune cannot take it as an argument
+    }
+}
