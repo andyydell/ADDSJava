80dcb8afa0c00a7830fd189e9d4a34cd6788eef2
andyydell <25mdellorfano@derryfield.org>
2025-04-18 17:01:59 -0400

    N04

---
+++ N04/ReadHandler.java
@@ -0,0 +1,28 @@
+package N04;
+
+import java.io.*;
+import java.net.*;
+
+public class ReadHandler implements Runnable {
+    private BufferedReader in;
+
+    public ReadHandler(Socket socket) {
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            System.out.println("Error setting up input stream.");
+        }
+    }
+
+  
+    public void run() {
+        String serverMessage;
+        try {
+            while ((serverMessage = in.readLine()) != null) {
+                System.out.println(serverMessage);
+            }
+        } catch (IOException e) {
+            System.out.println("Disconnected from server.");
+        }
+    }
+}
